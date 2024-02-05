552db24fc867bd9c3f8f74f66a407cbe939d0e3f
hunglq <104688414+1001VND@users.noreply.github.com>
2024-02-05 21:16:05 +0700

    Service Order with Clean Architechture

---
+++ src/Services/Ordering/Ordering.Application/Models/EmailSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Application.Models
+{
+	public class EmailSettings
+	{
+		public string Host { get; set; }
+		public int Port { get; set; }
+		public string User { get; set; }
+		public string Password { get; set; }
+	}
+}
